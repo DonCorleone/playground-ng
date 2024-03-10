3ee4c6ecbdad69d58021304832e5a487fd529564
Linus Wieland <linus@wieland.click>
2024-03-10 23:12:06 +0100

    call it baby

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {AsyncPipe, JsonPipe} from "@angular/common";
+import {Blog, BloggerService} from "./services/blogger.service";
+import {EMPTY, Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ng-17-2-3';
+  blogs$: Observable<Blog> = EMPTY
+
+  constructor(private bloggerService: BloggerService) {
+    this.blogs$ = this.bloggerService.blogs$;
+    const y = this.blogs$.subscribe((blog) => {
+      console.log(blog);
+    } );
+  }
 }
